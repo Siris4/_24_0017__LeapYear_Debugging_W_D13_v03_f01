c8b44eccba4c69beaa93f23ef79cc3b4db008b1c
Siris <97780529+Siris4@users.noreply.github.com>
2024-01-07 11:01:06 -0800

    Add files via upload

---
+++ _24_0017__LeapYear_Debugging_W_D13_v03_f01.py
@@ -0,0 +1,25 @@
+# Which year do you want to check?
+year = input()
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+
+
+#Which year do you want to check2?
+year = int(input())
+
+if year % 4 == 0:
+    if year % 100 != 0:
+        print("Leap year.")
+    elif year % 400 == 0:
+        print("Leap year.")
+    else:
+        print("Not leap year.")
